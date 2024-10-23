a921d6155566a190c5f32dfb163331c34246b4cf
mc-011g <55997399+mc-011g@users.noreply.github.com>
2024-10-23 09:00:01 -0600

    Updated App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders Chats heading', () => { 
+test('renders Login heading', () => { 
   render(<App />);
-  const linkElement = screen.getByText(/Chats/i);
+  const linkElement = screen.getByText('Login');
   expect(linkElement).toBeInTheDocument();
 });
