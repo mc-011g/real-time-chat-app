1a32611dd8326b12ca805b6bd0570ec380d91b20
Matthew Carmichael <mcarmichael011@gmail.com>
2025-05-08 06:13:31 -0600

    Setup new project

--- backend/src/main/java/com/example/real_time_chat_app/repository/RoomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.real_time_chat_app.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.example.real_time_chat_app.model.Room;
-
-/** Room repository */
-public interface RoomRepository extends MongoRepository<Room, String> {
-}
--- frontend/src/components/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { getUserRole } from '../services/jwtUtils.js';
-
- const ProtectedRoute = () => { 
-   return (getUserRole() ? <Outlet /> : <Navigate to={'/login'}/>);
- };
-
-export default ProtectedRoute;
